602089e37025b0fb1a1812b5a3f8fda8eedc6eb4
KurtReti <kurt.reti@gmail.com>
2022-10-27 15:06:49 +1100

    Update ResponsiveImage.jsx

--- src/components/ResponsiveImage.jsx
+++ src/components/ResponsiveImage.jsx
@@ -3,13 +3,13 @@ import React from "react";
 export default function ResponsiveImage (props) {
     return (
       <picture className="img">
-        <source className="img-mobile"
+        <source 
           media="(max-width: 1120px)"
           srcSet={`${props.imagemobile} 1120w`}
           sizes="1120px"
           width={`${props.mobilewidth}`}
         />
-        <source className="img-desktop"
+        <source 
           srcSet={`${props.imagedesktop} 1280w`}
           sizes="1280px"
           width={`${props.desktopwidth}`}
